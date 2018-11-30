829f4de4a0efca61b1dfd9182856db40cf02a046
MujtabaMamoun <44872348+MujtabaMamoun@users.noreply.github.com>
2018-11-30 07:59:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ function clean(text) {
         return text;
 }
 
-const prefix = "Your Prefix";
-const token = "Token Your Bot";
+const prefix = "!";
+const token = "NTE4MDkyNTU1NzcxMDUyMDMy.DuLvYA.dCsbtJXLe_Yuv631FHHDK7FuDk8";
 
 client.on("ready", () => {
   console.log("Vulnix | Logged in! Server count: ${client.guilds.size}");
@@ -86,4 +86,4 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 
 });
 
-client.login(token);
+client.login(NTE4MDkyNTU1NzcxMDUyMDMy.DuLvYA.dCsbtJXLe_Yuv631FHHDK7FuDk8);
